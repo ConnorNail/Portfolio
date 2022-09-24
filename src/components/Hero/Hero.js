b63c0eb67fcd6f2d45e01cd9a3a00f5d3ac9ee29
manff <nailboyz0247@cox.net>
2022-09-23 17:30:25 -0700

    Typo

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = (props) => (
           Hello,
         </SectionTitle>
         <SectionText>
-          My name is Connor Nail, I am a Robotics & Autonomous Systems graduate student enrolled at Arizona State University. I have a background in the CMP process of semiconductor manufacturing with experience in reliability testing, physics simulations, and web development.
+          My name is Connor Nail. I am a Robotics & Autonomous Systems graduate student enrolled at Arizona State University. I have a background in the CMP process of semiconductor manufacturing with experience in reliability testing, physics simulations, and web development.
         </SectionText>
         <Button onClick={(e) => {
           e.preventDefault();
